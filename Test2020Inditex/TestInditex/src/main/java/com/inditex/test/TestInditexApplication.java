66b26097de6f607df10ae08380ae49205f6764c1
InformaticJ <jorge.mora.campoy@gmail.com>
2023-03-31 15:39:41 +0200

    testear falta

--- Test2020Inditex/TestInditex/src/main/java/com/inditex/test/TestInditexApplication.java
+++ Test2020Inditex/TestInditex/src/main/java/com/inditex/test/TestInditexApplication.java
@@ -3,10 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-@EnableAutoConfiguration
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.inditex.test.infraestructura.config"})
 public class TestInditexApplication {
     public static void main(final String[] args) {
         SpringApplication.run(TestInditexApplication.class, args);
