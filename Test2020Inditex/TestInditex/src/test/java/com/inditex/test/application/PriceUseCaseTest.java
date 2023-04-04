1015b52b5c310a3f3195d1d7bd35f2e467141a63
InformaticJ <jorge.mora.campoy@gmail.com>
2023-04-04 09:33:24 +0200

    testeado

--- Test2020Inditex/TestInditex/src/test/java/com/inditex/test/application/PriceUseCaseTest.java
+++ Test2020Inditex/TestInditex/src/test/java/com/inditex/test/application/PriceUseCaseTest.java
@@ -4,6 +4,7 @@ import com.inditex.test.domain.Price;
 import com.inditex.test.infrastructure.entities.PriceEntity;
 import com.inditex.test.infrastructure.rest.mappers.PriceMapper;
 import com.inditex.test.infrastructure.rest.outputadapter.RepositoryPrice;
+import com.inditex.test.infrastructure.rest.outputadapter.RepositoryPriceH2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,8 @@ import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -33,14 +30,14 @@ import static org.mockito.Mockito.when;
 class PriceUseCaseTest {
 
     @Mock
-    RepositoryPrice repositoryPrice;
+    RepositoryPriceH2 repositoryPriceH2;
     @InjectMocks
     PriceUseCase priceUseCase;
-    List<PriceEntity> prices = new ArrayList<>();
-    PriceEntity price1 = new PriceEntity();
-    PriceEntity price2 = new PriceEntity();
-    PriceEntity price3 = new PriceEntity();
-    PriceEntity price4 = new PriceEntity();
+    List<Price> prices = new ArrayList<>();
+    Price price1 = new Price();
+    Price price2 = new Price();
+    Price price3 = new Price();
+    Price price4 = new Price();
 
     @BeforeEach
     void init() throws ParseException {
@@ -48,7 +45,7 @@ class PriceUseCaseTest {
         SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
         Date startDate1 = dateFormat.parse("2020-06-14-00.00.00");
         Date endDate1 = dateFormat.parse("2020-12-31-23.59.59");
-        price1 = PriceEntity.builder()
+        price1 = Price.builder()
                 .brandId(1)
                 .startDate(startDate1)
                 .endDate(endDate1)
@@ -60,7 +57,7 @@ class PriceUseCaseTest {
                 .build();
         Date startDate2 = dateFormat.parse("2020-06-14-15.00.00");
         Date endDate2 = dateFormat.parse("2020-06-14-18.30.00");
-        price2 = PriceEntity.builder()
+        price2 = Price.builder()
                 .brandId(1)
                 .startDate(startDate2)
                 .endDate(endDate2)
@@ -72,7 +69,7 @@ class PriceUseCaseTest {
                 .build();
         Date startDate3 = dateFormat.parse("2020-06-15-00.00.00");
         Date endDate3 = dateFormat.parse("2020-06-15-11.00.00");
-        price3 = PriceEntity.builder()
+        price3 = Price.builder()
                 .brandId(1)
                 .startDate(startDate3)
                 .endDate(endDate3)
@@ -84,7 +81,7 @@ class PriceUseCaseTest {
                 .build();
         Date startDate4 = dateFormat.parse("2020-06-15-16.00.00");
         Date endDate4 = dateFormat.parse("2020-12-31-23.59.59");
-        price4 = PriceEntity.builder()
+        price4 = Price.builder()
                 .brandId(1)
                 .startDate(startDate4)
                 .endDate(endDate4)
@@ -100,51 +97,58 @@ class PriceUseCaseTest {
         prices.add(price4);
     }
 
-
     @Test
     void getPricesTest1() throws ParseException {
         String pattern = ("yyyy-MM-dd HH:mm:ss");
         SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-        when(repositoryPrice.findByDateBetweenAndBrandIdAndProductId(any(),anyInt(), anyInt())).thenReturn(prices);
-        List<Price> priceList = priceUseCase.getPrices(dateFormat.parse("2020-12-31 23:59:58"), 35455, 1);
-        List<PriceEntity> priceListExpected = new ArrayList<>();
+        when(repositoryPriceH2.getPrices(any(),anyInt(), anyInt())).thenReturn(prices);
+        List<Price> priceList = priceUseCase.getPrices(dateFormat.parse("2020-06-14 10:00:00"), 35455, 1);
+        List<Price> priceListExpected = new ArrayList<>();
         priceListExpected.add(price1);
-        Assertions.assertEquals(PriceMapper.INSTANCE.priceEntityListToPriceList(priceListExpected), priceList);
+        Assertions.assertEquals(priceListExpected.get(0), priceList.get(0));
     }
-/*
+
     @Test
-    void getPricesTest2() {
-        when(repositoryPrice.findByProductIdAndBrandId(anyInt(), anyInt())).thenReturn(prices);
-        List<Price> priceList = servicePrice.getPrices("2020-06-14-16.00.00", 35455, 1);
+    void getPricesTest2() throws ParseException {
+        String pattern = ("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        when(repositoryPriceH2.getPrices(any(),anyInt(), anyInt())).thenReturn(prices);
+        List<Price> priceList = priceUseCase.getPrices(dateFormat.parse("2020-06-14 16:00:00"), 35455, 1);
         List<Price> priceListExpected = new ArrayList<>();
         priceListExpected.add(price2);
-        Assertions.assertEquals(priceListExpected, priceList);
+        Assertions.assertEquals(priceListExpected.get(0), priceList.get(1));
     }
 
     @Test
-    void getPricesTest3() {
-        when(repositoryPrice.findByProductIdAndBrandId(anyInt(), anyInt())).thenReturn(prices);
-        List<Price> priceList = servicePrice.getPrices("2020-06-14-21.00.00", 35455, 1);
+    void getPricesTest3() throws ParseException {
+        String pattern = ("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        when(repositoryPriceH2.getPrices(any(),anyInt(), anyInt())).thenReturn(prices);
+        List<Price> priceList = priceUseCase.getPrices(dateFormat.parse("2020-06-14 21:00:00"), 35455, 1);
         List<Price> priceListExpected = new ArrayList<>();
         priceListExpected.add(price1);
-        Assertions.assertEquals(priceListExpected, priceList);
+        Assertions.assertEquals(priceListExpected.get(0), priceList.get(0));
     }
 
     @Test
-    void getPricesTest4() {
-        when(repositoryPrice.findByProductIdAndBrandId(anyInt(), anyInt())).thenReturn(prices);
-        List<Price> priceList = servicePrice.getPrices("2020-06-15-10.00.00", 35455, 1);
+    void getPricesTest4() throws ParseException {
+        String pattern = ("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        when(repositoryPriceH2.getPrices(any(),anyInt(), anyInt())).thenReturn(prices);
+        List<Price> priceList = priceUseCase.getPrices(dateFormat.parse("2020-06-15 10:00:00"), 35455, 1);
         List<Price> priceListExpected = new ArrayList<>();
         priceListExpected.add(price3);
-        Assertions.assertEquals(priceListExpected, priceList);
+        Assertions.assertEquals(priceListExpected.get(0), priceList.get(2));
     }
 
     @Test
-    void getPricesTest5() {
-        when(repositoryPrice.findByProductIdAndBrandId(anyInt(), anyInt())).thenReturn(prices);
-        List<Price> priceList = servicePrice.getPrices("2020-06-14-21.00.00", 35455, 1);
+    void getPricesTest5() throws ParseException {
+        String pattern = ("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        when(repositoryPriceH2.getPrices(any(),anyInt(), anyInt())).thenReturn(prices);
+        List<Price> priceList = priceUseCase.getPrices(dateFormat.parse("2020-06-14 21:00:00"), 35455, 1);
         List<Price> priceListExpected = new ArrayList<>();
         priceListExpected.add(price1);
-        Assertions.assertEquals(priceListExpected, priceList);
-    }*/
+        Assertions.assertEquals(priceListExpected.get(0), priceList.get(0));
+    }
 }
